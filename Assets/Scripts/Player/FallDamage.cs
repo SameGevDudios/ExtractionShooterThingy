7b09dc1ffbf4990c91e6df033d0f7f0d6ad06b13
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-09 13:39:09 +0300

    Update FallDamage.cs

--- Assets/Scripts/Player/FallDamage.cs
+++ Assets/Scripts/Player/FallDamage.cs
@@ -4,13 +4,13 @@ public class FallDamage : MonoBehaviour
 {
     [SerializeField] private PlayerHealth _playerHealth;
     [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private float _vecolityThreshold;
+    [SerializeField] private float _velocityThreshold;
     private float _currentVelocity, _previousVelocity, _impactForce;
     private void Update()
     {
         _currentVelocity = _rigidbody.linearVelocity.y;
         _impactForce = Mathf.Abs(_currentVelocity - _previousVelocity);
-        if (_impactForce > _vecolityThreshold)
+        if (_impactForce > _velocityThreshold)
             // ShakeCamera(_impactForce);
         _previousVelocity = _rigidbody.linearVelocity.y;
     }
