e931d7a05ad703c2f18b50057baddd1e44c21dca
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-12 15:13:10 +0300

    Simplify camera shake code

--- Assets/Scripts/CameraShake/CameraShake.cs
+++ Assets/Scripts/CameraShake/CameraShake.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class CameraShake : MonoBehaviour
 {
     [SerializeField] private Transform _camTransform;
-    [SerializeField] private float _shakeStrength;
     #region Singleton
     public static CameraShake Instance;
     private void Awake() =>
@@ -12,16 +11,14 @@ public class CameraShake : MonoBehaviour
     public void ShakeCamera(Vector2 shake)
     {
         int direction = Random.Range(0, 2) > 0 ? 1 : -1;
-        float shakeX = _shakeStrength * shake.x * direction;
         // No muliplying by direction and another shake.y to avoid vertical downward shake
-        float shakeY = _shakeStrength * shake.y;
-        _camTransform.Rotate(Vector3.forward * shakeX + Vector3.right * shakeY);
+        _camTransform.Rotate(Vector3.forward * shake.x * direction + Vector3.right * shake.y);
     }
     public void ShakeCamera(float force)
     {
-        float shakeX = _shakeStrength * Random.Range(-force, force);
+        float shakeX = Random.Range(-force, force);
         // No muliplying by direction and another shake.y to avoid vertical downward shake
-        float shakeY = _shakeStrength * Random.Range(0, force);
+        float shakeY = Random.Range(0, force);
         _camTransform.Rotate(Vector3.forward * shakeX + Vector3.right * shakeY);
     }
 }
