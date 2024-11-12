10ebff4b53675f80bbe660c4f1d863ba682b8c3a
SameGevDudios <magomed.chapaev1@gmail.com>
2024-11-12 15:25:44 +0300

    Update SimpleSpring.cs

--- Assets/Scripts/Physics/SimpleSpring.cs
+++ Assets/Scripts/Physics/SimpleSpring.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class SimpleSpring : MonoBehaviour
 {
     [SerializeField] private float _springForce, _damping;
-    [SerializeField] private Vector3 targetPosition = new Vector3(0, 0, 0);
-    private Vector3 velocity;
-    void Update()
+    [SerializeField] private Vector3 _targetPosition;
+    private Vector3 _velocity;
+    private void Update()
     {
-        Vector3 springForce = (targetPosition - transform.localPosition) * _springForce;
-        Vector3 dampingForce = velocity * _damping;
+        Vector3 springForce = (_targetPosition - transform.localPosition) * _springForce;
+        Vector3 dampingForce = _velocity * _damping;
         Vector3 force = springForce - dampingForce;
-        velocity += force * Time.deltaTime;
-        transform.localPosition += velocity * Time.deltaTime;
+        _velocity += force * Time.deltaTime;
+        transform.localPosition += _velocity * Time.deltaTime;
     }
 }
