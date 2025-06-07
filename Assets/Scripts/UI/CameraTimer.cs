4c345939e4601f5483a33054a2f9420ad6c0db6c
SameGevDudios <magomed.chapaev1@gmail.com>
2025-06-07 12:41:15 +0300

    Optimize camera timer

--- Assets/Scripts/UI/CameraTimer.cs
+++ Assets/Scripts/UI/CameraTimer.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 using TMPro;
+using System.Text;
+
 public class CameraTimer : MonoBehaviour
 {
     [SerializeField] private TMP_Text _timerText;
-    [SerializeField] private float _timeElapsed;
+    private float _timeElapsed;
+    private StringBuilder _sb = new();
 
     void Update()
     {
         _timeElapsed += Time.deltaTime;
-
-        int minutes = Mathf.FloorToInt(_timeElapsed / 60F);
-        int seconds = Mathf.FloorToInt(_timeElapsed % 60F);
-        int milliseconds = Mathf.FloorToInt((_timeElapsed * 100F) % 100F);
-
-        _timerText.text = string.Format("{0:00}:{1:00}.{2:00}", minutes, seconds, milliseconds);
+        int seconds = Mathf.FloorToInt(_timeElapsed % 60f);
+        int milliseconds = Mathf.FloorToInt((_timeElapsed % 1f) * 100f);
+        int minutes = Mathf.FloorToInt(_timeElapsed / 60f);
+        _sb.Clear();
+        _sb.AppendFormat("{0:00}:{1:00}.{2:00}", minutes, seconds, milliseconds);
+        _timerText.text = _sb.ToString();
     }
 }
